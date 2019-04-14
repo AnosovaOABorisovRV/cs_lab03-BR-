4ad7f06af76441fc77f1bc6748a432587be650f7
Roman <myemail@list.ru>
2019-04-14 21:43:17 +0300

    code: Часть 2. Вывод гистограммы как изображения в формате SVG

--- main.cpp
+++ main.cpp
@@ -99,22 +99,70 @@ int show_histogram_text(vector <size_t> bins)
      }
 }
 
+///////////////////////////////////////////////
+void svg_begin(double width, double height)
+{
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg width='" << width << "' height='" << height  << "' "
+         << "viewBox='0 0 " << width << " " << height << "' "
+         << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void svg_end()
+{
+    cout << "</svg>\n";
+}
+
+void svg_text(double left, double baseline, string text)
+{
+    cout<<"<text x='"<<left<<"' y='"<<baseline<<"' >"<<text<<"</text>";
+}
+
+
+
+void svg_rect(double x, double y, double width, double height,string stroke="red",string fill="orange")
+{
+    cout<<" <rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />";
+}
+
+void show_histogram_svg(const vector<size_t>& bins)
+{
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    double top = 0;
+    svg_begin(400,300);
+    string stroke;
+    string fill;
+    for (size_t bin : bins)
+    {
+        const double bin_width = 10 * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,stroke,fill);
+        top += BIN_HEIGHT;
+    }
+    svg_end();
+}
+
 int main()
 {
     // Ввод данных
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
-    const vector<double> numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(number_count);
 
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
     // Обработка данных
-    const vector <size_t> bins = make_histogram(numbers,bin_count);
+    const auto bins = make_histogram(numbers,bin_count);
 
     // Вывод данных
-    show_histogram_text(bins);
-
-     return 0;
- }
+    //show_histogram_text(bins);
+    show_histogram_svg(bins);
+    return 0;
+}
