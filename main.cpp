114f6291dc02156c520310a11fa5aa9b5e1373e7
Roman <myemail@list.ru>
2019-04-15 03:27:30 +0300

    code: Часть 3-34. main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include "histogram.h"
 using namespace std;
 
 vector <double> input_numbers(size_t count)
@@ -13,31 +14,11 @@ vector <double> input_numbers(size_t count)
     return result;
 }
 
-void find_minmax(const vector<double>& numbers, double& min, double& max)
-{
-    min = numbers[0];
-    max = numbers[0];
-   for (double number : numbers)
-    {
-        if (number < min)
-        {
-             min = number;
-        }
-
-        if (number > max)
-        {
-             max = number;
-        }
-     }
-
-
-}
-
-vector <size_t> make_histogram(vector<double>numbers,size_t bin_count)
+vector <size_t> make_histogram(vector<double>numbers, size_t bin_count)
 {
     double min = numbers[0];
     double max = numbers[0];
-    find_minmax(numbers,min,max);
+    find_minmax(numbers, min, max);
     vector <size_t> bins(bin_count);
     for (double number : numbers)
     {
